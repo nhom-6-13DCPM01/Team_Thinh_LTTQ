26e5c8c5f6d36afcf9f008c8395a62d871c8ac70
thinhtruong2305 <56267717+thinhtruong2305@users.noreply.github.com>
2021-12-28 14:36:35 +0700

    Chỉnh sửa trang chính commit

--- QuanLiGiuXe/FormTrangChinh.Designer.cs
+++ QuanLiGiuXe/FormTrangChinh.Designer.cs
@@ -29,12 +29,69 @@ namespace QuanLiGiuXe
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartDoanhThu = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBoxNam = new System.Windows.Forms.ComboBox();
+            this.labelNam = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDoanhThu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartDoanhThu
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartDoanhThu.ChartAreas.Add(chartArea1);
+            this.chartDoanhThu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            legend1.Name = "Legend1";
+            this.chartDoanhThu.Legends.Add(legend1);
+            this.chartDoanhThu.Location = new System.Drawing.Point(0, 105);
+            this.chartDoanhThu.Name = "chartDoanhThu";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartDoanhThu.Series.Add(series1);
+            this.chartDoanhThu.Size = new System.Drawing.Size(800, 345);
+            this.chartDoanhThu.TabIndex = 0;
+            this.chartDoanhThu.Text = "chart1";
+            // 
+            // comboBoxNam
+            // 
+            this.comboBoxNam.FormattingEnabled = true;
+            this.comboBoxNam.Location = new System.Drawing.Point(61, 29);
+            this.comboBoxNam.Name = "comboBoxNam";
+            this.comboBoxNam.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxNam.TabIndex = 1;
+            // 
+            // labelNam
+            // 
+            this.labelNam.AutoSize = true;
+            this.labelNam.Location = new System.Drawing.Point(23, 32);
+            this.labelNam.Name = "labelNam";
+            this.labelNam.Size = new System.Drawing.Size(32, 13);
+            this.labelNam.TabIndex = 2;
+            this.labelNam.Text = "Năm:";
+            // 
+            // FormTrangChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelNam);
+            this.Controls.Add(this.comboBoxNam);
+            this.Controls.Add(this.chartDoanhThu);
+            this.Name = "FormTrangChinh";
             this.Text = "FormTrangChinh";
+            ((System.ComponentModel.ISupportInitialize)(this.chartDoanhThu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartDoanhThu;
+        private System.Windows.Forms.ComboBox comboBoxNam;
+        private System.Windows.Forms.Label labelNam;
     }
 }
