807a482114ec659645c6c58367b749f855e11584
Lekojj <ng.le.viet.00@gmail.com>
2021-12-28 08:28:37 +0700

    tao form xe vao

--- QuanLiGiuXe/fXeVao.cs
+++ QuanLiGiuXe/fXeVao.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLiGiuXe
 {
-    public partial class Form1 : Form
+    public partial class fXeVao : Form
     {
-        public Form1()
+        public fXeVao()
         {
             InitializeComponent();
         }
